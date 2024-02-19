0bdc0af9f4002d079d02787cddcfc51292eb2b4a
shaxana <bd7n9d7mw@code.edu.az>
2024-02-19 06:02:23 -0800

    added

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
-int main(void){
+#include <stdio.h>
 /**
  * main - entry point
  *
  * Return Always 0 (success)
  */
+int main(void){
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
