0ecd341f634058ac8982e37d660e61eeeaf2e361
shaxana <bd7n9d7mw@code.edu.az>
2024-02-19 06:09:46 -0800

    added

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void){
+printf("%s", "with proper grammar, but the outcome is a piece of art, \n");
+	return(0);
+}
