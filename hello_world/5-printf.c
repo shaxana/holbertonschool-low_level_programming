4e3278be48ee073d46c30cd06ea014381c0e5a53
shaxana <bd7n9d7mw@code.edu.az>
2024-02-19 06:12:55 -0800

    addeD

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: Always 0 (success)
  */
 int main (void){
-printf("%s", "with proper grammar, but the outcome is a piece of art, \n");
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
