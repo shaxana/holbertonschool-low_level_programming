983ffa66fc0ec619656c1ee716f7772fa3b399b5
shaxana <bd7n9d7mw@code.edu.az>
2024-02-22 14:12:15 +0000

    addeD

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,18 +4,13 @@
 /**
  * main - Prints the alphabet in lowercase.
  *
- * Return: Always (success)
+ * Return: Always 0.
  */
-
 int main(void)
 {
 char letter;
-
-
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
 putchar('\n');	
-
-
 return (0);
 }
