479eb158a9701526198fb46bfd149b087e1482d8
shaxana <bd7n9d7mw@code.edu.az>
2024-02-22 13:44:49 +0000

    addeD

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return Always (success)
+ */
+
+int main(void)
+{
+
+char C;
+
+
+for (C='A'; C<='z'; C++){
+putchar(C);
+}	
+	return (0);
+}
