090e6752858d555df98e41d773e3ffb4549b28d2
shaxana <bd7n9d7mw@code.edu.az>
2024-02-28 13:19:38 +0000

    added

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+  if(  c = 'A')
+return(1);
+printf("%c: %d\n", c, _isupper(c));
+    if(c = 'a')
+return(0);
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
