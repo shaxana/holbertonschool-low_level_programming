b36f4278e73b855d6505c999fe4cf4daf8f40910
shaxana <bd7n9d7mw@code.edu.az>
2024-02-28 14:36:35 +0000

    addeD

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _putchar - prints numbers from 0 to 9
+ * @c: number
+ * Return: numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+	for (c=0;c<=9;c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
