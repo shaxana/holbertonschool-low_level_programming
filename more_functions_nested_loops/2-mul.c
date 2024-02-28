bf31f96d504fa0e0a73f3076c79e79937c7f573b
shaxana <bd7n9d7mw@code.edu.az>
2024-02-28 14:08:26 +0000

    added

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _mul - check the code
- * @a,b: variables 
- * Return: Alwas 0.
+ * _mul - check the code.
+ * @a: first integer.
+ * @b:second integer. 
+ * Return: Always 0.
  */
 int mul(int a,int b)
 {
