3cb5e9558a8eafda1651d49b3a75d14d69b8f117
shaxana <bd7n9d7mw@code.edu.az>
2024-03-01 14:36:32 +0000

    addeD

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ *
+ * @n:variable
+ * Return: Always 0.
+ */ 
+void print_diagonal(int n)
+{
+	int i;
+	for(i = 0; i < n; i++)
+	{
+	if (n > 0)
+	{
+	_putchar('\');
+	}
+	}
+}
