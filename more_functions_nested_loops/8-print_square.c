0144318dea17292eee8d4f0ce19ade7323a4accb
shaxana <bd7n9d7mw@code.edu.az>
2024-03-02 20:28:54 +0000

    Added

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,13 +6,13 @@
  */
 void print_square(int size)
 {
-	int i , j;
+	int i, j;
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 			_putchar('#');
-	if(i != size - 1)
+	if (i != size - 1)
 		_putchar('\n');
 	}
-	_putchar('\n');
+	_putchar('\n')i
 }
