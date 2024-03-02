3ad051bd56f680f8956d6ad62a0a6bba77099de8
shaxana <bd7n9d7mw@code.edu.az>
2024-03-02 20:20:29 +0000

    added

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,20 +6,21 @@
  *     */
 void print_diagonal(int n)
 {
-		int i = 0, j = 0;
 
-			while (n > i)
-					{
-								while (i > j)
-											{
-															_putchar(32);
-																		j++;
-																				}
-										_putchar(92);
-												if (i != n - 1)
-																_putchar('\n');
-														j = 0;
-																i++;
-																	}
-				_putchar('\n');
+	int i = 0, j = 0;
+
+	while (n > i)
+	{
+		while (i > j)
+		{
+		_putchar(32);
+		j++;
+		}
+	_putchar(92);
+	if (i != n - 1)
+		_putchar('\n');
+	j = 0;
+	i++;
+	}
+	_putchar('\n');
 }
