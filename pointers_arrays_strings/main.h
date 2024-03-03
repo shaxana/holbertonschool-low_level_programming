6f4a69990f00b3f9a5e3a47a82fd107512179f9f
shaxana <bd7n9d7mw@code.edu.az>
2024-03-03 09:18:56 +0000

    :Added

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,3 +10,11 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
