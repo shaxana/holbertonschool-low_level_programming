33662626c634a4052aa5a26359b2b806a64782b7
shaxana <bd7n9d7mw@code.edu.az>
2024-03-05 10:28:23 +0000

    added

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsinged int size)
+{
+	unsigned int i;
+	i = 0;
+
+	while (i < n)
+	{
+		*s++ = buffer;
+		i++;
+	}
+	return (buffer);
+}
