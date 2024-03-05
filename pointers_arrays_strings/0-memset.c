a46a0cc01398109594ab9bc125cf3322b971aad2
shaxana <bd7n9d7mw@code.edu.az>
2024-03-05 20:31:34 +0000

    AddeD

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,7 +6,6 @@
  * @s: pointer to start of memory area
  * @b: constant byte to fill with
  * @n: num bytes to fill in 
- *
  * Return: pointer to beginning of memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
