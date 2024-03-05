09f03f4bdd249c73cf6fc1c281b863f1e0654f20
shaxana <bd7n9d7mw@code.edu.az>
2024-03-05 21:45:52 +0000

    AddeD

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character in string
+ *
+ * Return: character in string
+ */
+char *_strchr(char *s, char c)
+{
+	int i =  0;
+
+	for(i = 0; i < n; i++)
+	{
+		if (c == i)
+		{
+			strchr(s, i);
+		}
+	}
+	return (c);
+	
+}
