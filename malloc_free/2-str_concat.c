81e7bc4f77a02be29da2f862924d17a00eeeee25
shaxana <bd7n9d7mw@code.edu.az>
2024-03-16 20:34:39 +0000

    adde

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,32 +19,27 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = len2 = 0;
 
+
 	if (s1 != NULL)
 	{
-
 		i = 0;
 		while (s1[i++] != '\0')
 			len1++;
-
 	}
-	if (s2 != NULL)							
-	{
 
-	
+
+	if (s2 != NULL)
+	{
 		i = 0;
 		while (s2[i++] != '\0')
 			len2++;
 	}
 
 
-
 	len = len1 + len2;
-
-
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (result == NULL)
 		return (NULL);
-
 
 
 	for (i = 0; i < len1; i++)
@@ -52,7 +47,6 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; j < len2; j++, i++)
 		result[i] = s2[j];
 	result[len] = '\0';
-
 
 
 	return (result);
